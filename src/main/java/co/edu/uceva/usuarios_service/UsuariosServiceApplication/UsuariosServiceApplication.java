be689fb2725f9b13e53755a0e79298310354ae8e
Yilver <yilver.rivera01@uceva.edu.co>
2023-04-10 18:50:28 -0500

    .

---
+++ src/main/java/co/edu/uceva/usuarios_service/UsuariosServiceApplication/UsuariosServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.usuarios_service.UsuariosServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosServiceApplication.class, args);
+	}
+
+}
