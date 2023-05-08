f58d12a9b2c882d2109f0508e3aec0e66f35a8a6
Juan Manuel Carvajal <jmcr-@hotmail.com.ar>
2023-05-07 22:04:41 -0500

    se eliminaron archivos que de momento no hacen nada

--- src/main/java/co/edu/uceva/usuarios_service/UsuariosServiceApplication/model/dao/INotificaionDao.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uceva.usuarios_service.UsuariosServiceApplication.model.dao;
-
-import co.edu.uceva.usuarios_service.UsuariosServiceApplication.model.entities.Notificacion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface INotificaionDao extends CrudRepository<Notificacion, Long> {
-
-}
