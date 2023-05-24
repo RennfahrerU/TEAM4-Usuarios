f77ccf186e4fca49779d5cde60f4b6b3f6fd8062
Yilver <yilver.rivera01@uceva.edu.co>
2023-05-24 13:48:33 -0500

    Se eliminaron algunos valores el import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,7 @@ INSERT INTO usuario (nombre) VALUES ('Pablo');
 INSERT INTO usuario (nombre) VALUES ('Carlos');
 INSERT INTO usuario (nombre) VALUES ('Carlos');
 INSERT INTO usuario (nombre) VALUES ('Carlos');
+INSERT INTO usuario (nombre) VALUES ('Pablo');
+INSERT INTO usuario (nombre) VALUES ('Pedro');
+INSERT INTO usuario (nombre) VALUES ('Paco');
+INSERT INTO usuario (nombre) VALUES ('Pablo');
