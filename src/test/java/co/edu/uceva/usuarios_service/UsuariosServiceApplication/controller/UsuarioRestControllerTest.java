835214619bbd938827d09ccab2c5e339eb0b8107
Juan Manuel Carvajal Rada <jmcr-@hotmail.com.ar>
2023-06-04 00:49:09 -0500

    Se implemento el test Delete usuario, estado: funcional

--- src/test/java/co/edu/uceva/usuarios_service/UsuariosServiceApplication/controller/UsuarioRestControllerTest.java
+++ src/test/java/co/edu/uceva/usuarios_service/UsuariosServiceApplication/controller/UsuarioRestControllerTest.java
@@ -16,9 +16,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import static org.mockito.Mockito.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.Assert.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,9 +29,10 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
- public class UsuarioRestControllerTest {
+public class UsuarioRestControllerTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -39,12 +43,12 @@ import static org.junit.jupiter.api.Assertions.*;
     private IUsuarioService usuarioService;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
     @Test
-    public void testListarUsuario() throws Exception{
+    public void testListarUsuario() throws Exception {
         // Datos de prueba
         Usuario usuario1 = new Usuario();
         usuario1.setNombre("Carlos");
@@ -74,7 +78,7 @@ import static org.junit.jupiter.api.Assertions.*;
                 .andExpect(jsonPath("$[9].apellido", is(usuario2.getApellido())))
                 .andExpect(jsonPath("$[9].email", is(usuario2.getEmail())))
                 .andExpect(jsonPath("$[9].contrasena", is(usuario2.getContrasena())));
-                System.out.println((jsonPath("$[9].nombre", is(usuario1.getNombre()))));
+        System.out.println((jsonPath("$[9].nombre", is(usuario1.getNombre()))));
         // Eliminar los usuarios del servicio
         usuarioService.delete(usuario1);
         usuarioService.delete(usuario2);
@@ -101,7 +105,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
         // Obtiene la respuesta y verifica que el resultado sea correcto
         String response1 = result1.getResponse().getContentAsString();
-        List<Usuario> usuariosEncontrados1 = new ObjectMapper().readValue(response1, new TypeReference<List<Usuario>>() {});
+        List<Usuario> usuariosEncontrados1 = new ObjectMapper().readValue(response1, new TypeReference<List<Usuario>>() {
+        });
 
         // Verifica que se encuentre el usuario con nombre "Carlos" y apellido "Perez" en los resultados
         boolean carlosEncontrado1 = usuariosEncontrados1.stream()
@@ -115,7 +120,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
         // Obtiene la respuesta y verifica que el resultado sea correcto
         String response2 = result2.getResponse().getContentAsString();
-        List<Usuario> usuariosEncontrados2 = new ObjectMapper().readValue(response2, new TypeReference<List<Usuario>>() {});
+        List<Usuario> usuariosEncontrados2 = new ObjectMapper().readValue(response2, new TypeReference<List<Usuario>>() {
+        });
 
         // Verifica que se encuentre el usuario con nombre "Carlos" y apellido "Pérez" en los resultados
         boolean carlosEncontrado2 = usuariosEncontrados2.stream()
@@ -129,7 +135,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
         // Obtiene la respuesta y verifica que el resultado sea correcto
         String response3 = result3.getResponse().getContentAsString();
-        List<Usuario> usuariosEncontrados3 = new ObjectMapper().readValue(response3, new TypeReference<List<Usuario>>() {});
+        List<Usuario> usuariosEncontrados3 = new ObjectMapper().readValue(response3, new TypeReference<List<Usuario>>() {
+        });
 
         // Verifica que se encuentre el usuario con nombre "Carlos" y apellido "Pérez" en los resultados
         boolean carlosEncontrado3 = usuariosEncontrados3.stream()
@@ -140,6 +147,26 @@ import static org.junit.jupiter.api.Assertions.*;
         usuarioService.delete(user);
     }
 
+    /**
+     * Compobación del metodo Delete "/usuario-service/eliminarusuario/{id}", este tendra como
+     * objetivo verificar la eliminación de un usuario
+     * @throws Exception
+     */
+    @Test
+    public void testBorrarUsuario() throws Exception {
+        // Se crea el usuario para la prueba
+        Usuario user = new Usuario();
+        user.setNombre("Carlos");
+        user.setApellido("Perez");
+        user.setEmail("carlitos12@hotmail.com");
+        user.setContrasena("carlos123");
+        usuarioService.save(user);
+        //Se obtiene el id del usuario para posteriormente eliminarlo
+        this.mockMvc.perform(delete("/usuario-service/eliminarusuario/{id}", user.getId()))
+                .andExpect(status().isOk());
+        //se verifica la eliminación del usuario si retorna null el test esta correcto
+        assertNull(usuarioService.findById(user.getId()));
+    }
 
 
 }
